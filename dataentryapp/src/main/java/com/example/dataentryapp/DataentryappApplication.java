68fc8af822f9833b8da2e00f51745ac46597f476
hhussain119 <heshamhussain@outlook.com>
2025-02-22 18:35:04 -0500

    boiler plate spring project

---
+++ dataentryapp/src/main/java/com/example/dataentryapp/DataentryappApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataentryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataentryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataentryappApplication.class, args);
+	}
+
+}
---
+++ dataentryapp/src/test/java/com/example/dataentryapp/DataentryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataentryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataentryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
